3e2c339928b60bac716de1689f0b458b155e8d47
suha <suha@sookmyung.ac.kr>
2024-07-02 11:17:12 +0900

    feat: user REST API 구현

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('User API')
+    .setDescription('This is a sample REST API')
     .setVersion('1.0')
-    .addTag('cats')
+    // .addTag('users')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
